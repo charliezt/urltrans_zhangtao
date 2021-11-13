e4e70ab3ddf6384caf7a969cf1af7deeebcbd241
18512203311@126.com <18512203311@126.com>
2021-11-13 23:39:24 +0800

    Signed-off-by: 18512203311@126.com <18512203311@126.com>

---
+++ src/test/java/com/example/urltrans_zhangtao/SwaggerKaoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.urltrans_zhangtao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerKaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
